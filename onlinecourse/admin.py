46e34f9a36fef8e6b81bd42842c788f64ad0a4f3
mark29-prog <afriyiemark@gmail.com>
2026-04-22 08:53:34 -0400

    Fix admin.py: cleaned models registration and added Question, Choice, Submission admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,69 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
+# -----------------------
+# Inline classes
+# -----------------------
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# -----------------------
+# Course Admin
+# -----------------------
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
+    inlines = [LessonInline, QuestionInline]
+    list_display = ['name', 'pub_date']
     search_fields = ['name', 'description']
 
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+# -----------------------
+# Question Admin
+# -----------------------
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content', 'course', 'grade']
+    search_fields = ['content']
+    list_filter = ['course']
 
 
-# <HINT> Register Question and Choice models here
+# -----------------------
+# Choice Admin
+# -----------------------
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['text', 'question', 'is_correct']
+    list_filter = ['question', 'is_correct']
+    search_fields = ['text']
 
+
+# -----------------------
+# Submission Admin
+# -----------------------
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ['enrollment', 'submitted_at']
+    filter_horizontal = ['choices']
+    list_filter = ['enrollment']
+
+
+# -----------------------
+# Register models (CLEAN)
+# -----------------------
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Lesson)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Submission, SubmissionAdmin)
